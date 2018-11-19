ca4414075e66f5a3c9877e0f968370e03d5e8a8a
Abhisek <mail2abhisek.maiti@gmail.com>
2018-11-19 23:18:36 +0530

    PostgreSQL

--- TLE.php
+++ TLE.php
@@ -1,5 +1,11 @@
 <?php
-  $db = new SQLite3('Sat_Repo.db');
+  $host        = "host = 127.0.0.1";
+  $port        = "port = 5432";
+  $dbname      = "dbname = postgres";
+  $credentials = "user = postgres password=postgres";
+
+  $db = pg_connect( "$host $port $dbname $credentials"  );
+  // $db = new SQLite3('Sat_Repo.db');
   $satno = $_POST["satno"];
   $query_string = "SELECT RAW_TLE FROM Sat_Info WHERE SATELLITE_NUMBER = ".$satno;
   $results = $db->query($query_string);
