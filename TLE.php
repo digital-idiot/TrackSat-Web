e02f638123511991e8e3b9815bd76b5428917dc5
montimaj <monti.majumdar@gmail.com>
2018-11-21 17:41:02 +0530

    Upgraded to PostgreSQL

--- TLE.php
+++ TLE.php
@@ -1,21 +1,20 @@
 <?php
   $host        = "host = 127.0.0.1";
   $port        = "port = 5432";
-  $dbname      = "dbname = postgres";
+  $dbname      = "dbname = tracksat";
   $credentials = "user = postgres password=postgres";
 
-  $db = pg_connect( "$host $port $dbname $credentials"  );
-  // $db = new SQLite3('Sat_Repo.db');
-  $satno = $_POST["satno"];
-  $query_string = "SELECT RAW_TLE FROM Sat_Info WHERE SATELLITE_NUMBER = ".$satno;
-  $results = $db->query($query_string);
-  $rows = array();
-  while ($row = $results->fetchArray()) {
-    $rows[] = $row[0];
-  }
-  if (empty($rows)) {
-	  echo json_encode("No record found");
-  } else {
-		echo json_encode($rows);
+  $db = pg_connect("$host $port $dbname $credentials");
+  if ($db) {
+	  $satno = $_POST["satno"];
+          $query_string = "SELECT SATELLITE_NAME, RAW_TLE FROM public.Sat_Info WHERE SATELLITE_NUMBER = ".$satno;
+	  $results = pg_query($db, $query_string);
+	  $rows = array();
+	  while ($row = pg_fetch_row($results)) {
+	    $rows[] = $row;
+	  }
+	  if (empty($rows)) {
+		  echo json_encode("No record found");
+	  } else echo json_encode($rows);
   }
 ?>
